76a2f606151e2514a00b65e776de7eaa401af5f7
ShadyWestHaze <99255149+ShadyWestHaze@users.noreply.github.com>
2022-02-09 18:52:03 +0100

    Add files via upload

---
+++ Engineers paste the code here/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GXPEngine;
+
+public class Enemy : Sprite
+{
+    float speedX;
+    float speedY;
+    int health;
+    
+    public Enemy() : base("circle.png")
+    {
+        SetOrigin(width/2,height/2);
+        speedX = 1.0f;
+        speedY = 1.0f;
+         health = 1;
+        
+    }
+
+    public void EnemyGone()
+    {
+        this.LateDestroy();
+    }
+    void Update()
+    {
+        FindPlayer();
+    }
+
+    void FindPlayer()
+    {
+        x = x + speedX;
+        y = y + speedY;
+        speedX = speedX * 0.9f;
+        speedY = speedY * 0.9f;
+        
+    }
+}
