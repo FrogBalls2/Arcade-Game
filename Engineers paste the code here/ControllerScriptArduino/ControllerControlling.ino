076469a4d6e4e884bd0956686008dbbc1ecce320
FrogBalls2 <94798605+FrogBalls2@users.noreply.github.com>
2022-02-16 02:44:35 +0100

    Update ControllerControlling.ino

--- Engineers paste the code here/ControllerScriptArduino/ControllerControlling.ino
+++ Engineers paste the code here/ControllerScriptArduino/ControllerControlling.ino
@@ -1,10 +1,20 @@
 #include<Keyboard.h>
 
+//config buttons
+
 int ATK_BTN = 7;
 int W_BTN = 6;
 int A_BTN = 5;
 int S_BTN = 4;
 int D_BTN = 3;
+
+//config button states
+
+int STATE_ATTACK = 0;
+int STATE_W = 0;
+int STATE_A = 0;
+int STATE_S = 0;
+int STATE_D = 0;
 
 void setup() {
   //// connected to ground:
@@ -17,20 +27,45 @@ void setup() {
 }
 
 void loop() {
-  //if the button is pressed
-  if (digitalRead(D_BTN) == HIGH) {
-    Keyboard.write('D');
+  //if the button is pressed, act like a keyboard
+
+  if (digitalRead(W_BTN) == HIGH && STATE_W == LOW ) {
+    STATE_W = HIGH;
+    Keyboard.press('W');
+  }  else if (digitalRead(W_BTN) == LOW && STATE_W == HIGH) {
+    STATE_W = LOW;
+    Keyboard.release('W');
   }
-  if (digitalRead(S_BTN) == HIGH) {
-    Keyboard.write('S');
+
+   if (digitalRead(A_BTN) == HIGH && STATE_A == LOW ) {
+    STATE_A = HIGH;
+    Keyboard.press('A');
+  }  else if (digitalRead(A_BTN) == LOW && STATE_A == HIGH) {
+    STATE_A = LOW;
+    Keyboard.release('A');
   }
-  if (digitalRead(A_BTN) == HIGH) {
-    Keyboard.write('A');
+
+   if (digitalRead(S_BTN) == HIGH && STATE_S == LOW ) {
+    STATE_S = HIGH;
+    Keyboard.press('S');
+  }  else if (digitalRead(S_BTN) == LOW && STATE_S == HIGH) {
+    STATE_S = LOW;
+    Keyboard.release('S');
   }
-  if (digitalRead(W_BTN) == HIGH) {
-    Keyboard.write('W');
+
+ if (digitalRead(D_BTN) == HIGH && STATE_D == LOW ) {
+    STATE_D = HIGH;
+    Keyboard.press('D');
+  }  else if (digitalRead(D_BTN) == LOW && STATE_D == HIGH) {
+    STATE_D = LOW;
+    Keyboard.release('D');
   }
-  if (digitalRead(ATK_BTN) == HIGH) {
-    Keyboard.write('E');
+  
+  if (digitalRead(ATK_BTN) == HIGH && STATE_ATTACK == LOW ) {
+    STATE_ATTACK = HIGH;
+    Keyboard.press(' ');
+  }  else if (digitalRead(ATK_BTN) == LOW && STATE_ATTACK == HIGH) {
+    STATE_ATTACK = LOW;
+    Keyboard.release(' ');
   }
 }
